9e1b4e153e773959f9077cce37934c0d668f9436
Jose David Florez Ruiz <89201134+J0FR@users.noreply.github.com>
2022-08-18 22:03:33 -0500

    cambios

---
+++ test.py
@@ -0,0 +1,15 @@
+filename = 'files/test.txt'
+
+rules = {}
+
+with open(filename) as f:
+    lines = f.read().splitlines()
+
+for i in lines:
+    if(len(i)==0):
+        lines.remove(i)
+
+for line in lines:
+    print(line)
+
+print(lines)
